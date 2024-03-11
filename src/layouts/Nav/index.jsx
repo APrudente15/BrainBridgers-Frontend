38b7a69558655ceb0fa95dea7d40b13b17824ba8
APrudente15 <110691505+APrudente15@users.noreply.github.com>
2024-03-11 16:23:37 +0000

    change logo src URL

--- src/layouts/Nav/index.jsx
+++ src/layouts/Nav/index.jsx
@@ -63,7 +63,7 @@ const Nav = ({ logged, setLogged }) => {
                         <NavLink to='/discover'><div className='nl'>Discover</div></NavLink>
                     </div>
                 </motion.nav>
-                <div className='logo2'><img className='img' src='https://github-production-user-asset-6210df.s3.amazonaws.com/110691505/310489420-cb6409c2-fb5f-4caf-927b-394d357a8081.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240306%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240306T121134Z&X-Amz-Expires=300&X-Amz-Signature=0218988590c7e76d7f3134ef387d8096aecd095e54e578a02adeecdb3b6fce73&X-Amz-SignedHeaders=host&actor_id=152859842&key_id=0&repo_id=768040580' alt='logo' /></div>
+                <div className='logo2'><img className='img' src='https://github.com/APrudente15/BrainBridgersLogo/assets/110691505/cb6409c2-fb5f-4caf-927b-394d357a8081' alt='logo' /></div>
                 <Outlet />
                 <div>
                     <img onClick={dispInfo} className='info' src='https://cdn-icons-png.flaticon.com/512/8/8201.png' />
